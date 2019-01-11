eb7dc01a7143e8c5675a6ce4807bde01a6f342c2
Marius Berger <mberger@evoila.de>
2019-01-11 12:49:44 +0100

    refactor(KafkaJsonProducer): Refactor producer to use Jackson, a group id and offer methods for serialization of POJOs.

--- src/main/java/de/evoila/cf/autoscaler/kafka/producer/KafkaJsonProducer.java
+++ src/main/java/de/evoila/cf/autoscaler/kafka/producer/KafkaJsonProducer.java
@@ -1,12 +1,21 @@
 package de.evoila.cf.autoscaler.kafka.producer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import de.evoila.cf.autoscaler.kafka.KafkaPropertiesBean;
+import de.evoila.cf.autoscaler.kafka.messages.ApplicationMetric;
+import de.evoila.cf.autoscaler.kafka.messages.ContainerMetric;
+import de.evoila.cf.autoscaler.kafka.messages.HttpMetric;
+import de.evoila.cf.autoscaler.kafka.messages.ScalingLog;
 import de.evoila.cf.autoscaler.kafka.model.BindingInformation;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Service;
 
@@ -19,18 +28,19 @@ import java.util.Properties;
 @ConditionalOnBean(KafkaPropertiesBean.class)
 public class KafkaJsonProducer {
 
+    private static Logger log = LoggerFactory.getLogger(KafkaJsonProducer.class);
+
     private KafkaPropertiesBean kafkaProperties;
 
-    private Gson gson;
+    private ObjectMapper objectMapper;
 
-    Producer<String, String> producer;
+    private Producer<String, String> producer;
 
     public KafkaJsonProducer(KafkaPropertiesBean kafkaProperties) {
         this.kafkaProperties = kafkaProperties;
-        this.gson = new Gson();
-    }
 
-    public void initializeKafkaProducer() {
+        objectMapper = new ObjectMapper();
+
         Properties properties = new Properties();
         properties.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
         properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
@@ -38,14 +48,73 @@ public class KafkaJsonProducer {
         properties.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, 1000);
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaProperties.getProducerGroupId() == null ? "default-producer-group-id" : kafkaProperties.getProducerGroupId());
 
         producer = new KafkaProducer<>(properties);
     }
 
-    public void produceKafkaMessage(String topic, BindingInformation bindingInformation) {
-        ProducerRecord<String, String> record = new ProducerRecord<>(topic, gson.toJson(bindingInformation));
+    /**
+     * Publish a {@linkplain ScalingLog} on Kafka.
+     * @param scalingLog {@linkplain ScalingLog} to publish
+     */
+    public void produceScalingLog(ScalingLog scalingLog) {
+        produce(kafkaProperties.getScalingTopic(), scalingLog);
+    }
 
+    /**
+     * Publish a {@linkplain HttpMetric} on Kafka on the given topic.
+     * @param metric {@linkplain HttpMetric} to publish
+     */
+    public void produceHttpMetric(HttpMetric metric) {
+        produce(kafkaProperties.getMetricHttpTopic(), metric);
+    }
+
+    /**
+     * Publish a {@linkplain ContainerMetric} on Kafka.
+     * @param containerMetric {@linkplain ContainerMetric} to publish
+     */
+    public void produceContainerMetric(ContainerMetric containerMetric) {
+        produce(kafkaProperties.getMetricContainerTopic(), containerMetric);
+    }
+
+    /**
+     * Publish a {@linkplain ApplicationMetric} on Kafka.
+     * @param applicationMetric {@linkplain ApplicationMetric} to publish
+     */
+    public void produceApplicationMetric(ApplicationMetric applicationMetric) {
+        produce(kafkaProperties.getMetricApplicationTopic(), applicationMetric);
+    }
+
+    /**
+     * Publish a {@linkplain BindingInformation} on Kafka.
+     * @param bindingInformation {@linkplain BindingInformation} to publish
+     */
+    public void produceBindingInformation(BindingInformation bindingInformation) {
+        produce(kafkaProperties.getBindingTopic(), bindingInformation);
+    }
+
+    /**
+     * Serializes an object to JSON and then calls {@linkplain #produceString(String, String)}.
+     * @param topic Kafka topic to publish on
+     * @param jacksonSerializableObject object to serialize to JSON
+     */
+    public void produce(String topic, Object jacksonSerializableObject) {
+        try {
+            String json = objectMapper.writeValueAsString(jacksonSerializableObject);
+            produceString(topic, json);
+        } catch (JsonProcessingException ex) {
+            log.error("Failed to generate json from "+jacksonSerializableObject.getClass().getSimpleName()+" object.", ex);
+        }
+    }
+
+    /**
+     * Publishs a message in the String format on the given topic.
+     * Other methods in this class use this to publish JSON messages.
+     * @param topic Kafka topic to publish on
+     * @param message message to publish
+     */
+    public void produceString(String topic, String message) {
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, message);
         producer.send(record);
-        producer.flush();
     }
 }
